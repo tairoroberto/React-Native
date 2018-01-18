9921e3f2aa46d90d2533f2581a8c719a531e329f
tairoroberto <tairoroberto@hotmail.com>
2018-01-18 16:59:36 -0200

    HelloWorls react-native

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import {AppRegistry} from 'react-native';
+import HelloWorldApp from './HelloWorldApp';
 
-AppRegistry.registerComponent('HelloWorld', () => App);
+AppRegistry.registerComponent('HelloWorld', () => HelloWorldApp);
